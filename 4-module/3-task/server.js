be16124baa45a786d1156788c5bb24956b1ce4db
HknaMtata <lenka.hakuna.matata@gmail.com>
2020-03-24 12:10:03 +0500

    add 4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,7 +12,25 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
-
+      if (pathname.includes('/') || pathname.includes('..')) {
+        res.statusCode = 400;
+        res.end('Nested paths are not allowed');
+        break;
+      }
+      fs.unlink(filepath, (error) => {
+        if (error) {
+          if (error.code === 'ENOENT') {
+            res.statusCode = 404;
+            res.end('Not found');
+          } else {
+            res.statusCode = 500;
+            res.end('Internal error');
+          }
+        } else {
+          res.statusCode = 200;
+          res.end('Ok');
+        }
+      });
       break;
 
     default:
