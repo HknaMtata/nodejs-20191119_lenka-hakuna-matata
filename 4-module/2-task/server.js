d418d6add5b7e55e2e0561e189fbe5bad2b7baab
HknaMtata <lenka.hakuna.matata@gmail.com>
2020-03-24 11:50:00 +0500

    add 4-module 2-task

--- 4-module/2-task/server.js
+++ 4-module/2-task/server.js
@@ -1,6 +1,8 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
@@ -11,6 +13,52 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
+      if (pathname.includes('/') || pathname.includes('..')) {
+        res.statusCode = 400;
+        res.end('Nested paths are not allowed');
+        break;
+      }
+
+      const file = new fs.WriteStream(filepath, {flags: 'wx'});
+      file
+        .on('error', function(error) {
+          if (error.code === 'EEXIST') {
+            res.statusCode = 409;
+            res.end('File exists');
+          } else {
+            res.statusCode = 500;
+            res.end('Internal server error');
+            fs.unlink(filepath, (error) => {});
+          }
+        })
+        .on('close', function(){
+          res.statusCode = 201;
+          res.end('file has been saved');
+        });
+      
+      let limitStream = new LimitSizeStream({limit: 1e6});
+      limitStream
+      .on('error', function(error) {
+        if (error.code === 'LIMIT_EXCEEDED') {
+          res.statusCode = 413;
+          res.end('File is too big');
+        } else {
+          res.statusCode = 500;
+          res.end('Internal server error');
+        }
+        fs.unlink(filepath, (err) => {});
+      });
+
+      // Если обрыв соединения
+      res
+      .on('close', function(){
+        if (res.finished) return;
+        fs.unlink(filepath, (error) => {});
+      });
+
+      req
+        .pipe(limitStream)
+        .pipe(file);
 
       break;
 
